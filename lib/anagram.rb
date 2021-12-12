52220217136082867168f5cc953360f694115f67
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2021-12-12 12:43:20 -0600

    commit tree d14505b2b91dd0d8de5362e60532b2f0204b9044

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+        array.filter { |arr_word| arr_word.split("").sort == @word.split("").sort }
+    end
+end
